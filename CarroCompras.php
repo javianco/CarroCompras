802c46a7a01c1d065639d3765e157876efb464b4
javianco <javianco1965@gmail.com>
2022-02-24 11:43:25 -0500

    nueva version

--- CarroCompras.php
+++ CarroCompras.php
@@ -6,7 +6,7 @@
 	$idcleinte= 1;
 	$ncliente="";
 
-  
+
 
 	$sql="SELECT nombre,apellido 
 	FROM usuario WHERE id_usuario='$idcleinte'";
@@ -18,7 +18,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>E-comerce </title>
+<title>E-comerce Andrade Asociados</title>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
